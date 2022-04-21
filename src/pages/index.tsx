1df4e8e17aa30cc745c8b160f57cd1ca1c63880f
EduuardoPV <eduardoveltroni@hotmail.com>
2022-04-21 20:46:53 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Template | React Next</title>
+        <meta name="description" content="Description site" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+};
+
+export default Home;
